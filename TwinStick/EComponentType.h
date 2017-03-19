5282da43dcd482d4c8311626108c5734461e5880
Simon Johanson <johanson.simon@hotmail.com>
2017-03-19 16:39:52 +0100

    Added IComponent and EComponentType.

---
+++ TwinStick/EComponentType.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <memory>
+
+enum class EComponentType : std::uint32_t
+{
+	None		= 0,
+	Transform	= 1 << 0,
+	Movement	= 1 << 1,
+	Collider	= 1 << 2,
+	Render		= 1 << 3
+
+};
+
+using T = std::underlying_type_t<EComponentType>;
+
+inline EComponentType operator|( EComponentType lhs, EComponentType rhs )
+{
+	return static_cast<EComponentType>( static_cast<T>( lhs ) | static_cast<T>( rhs ) );
+
+}
+
+inline EComponentType operator|( unsigned int lhs, EComponentType rhs )
+{
+	return static_cast<EComponentType>( static_cast<T>( lhs ) | static_cast<T>( rhs ) );
+
+}
+
+
+inline EComponentType operator&( EComponentType lhs, EComponentType rhs )
+{
+	return static_cast<EComponentType>( static_cast<T>(lhs) & static_cast<T>( rhs ) );
+
+}
+
+inline EComponentType operator&( unsigned int lhs, EComponentType rhs )
+{
+	return static_cast<EComponentType>( static_cast<T>(lhs) & static_cast<T>( rhs ) );
+
+}
---
+++ TwinStick/IComponent.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <memory>
+#if _DEBUG
+	#include <iostream>
+#endif
+#include "EComponentType.h"
+
+
+__interface IComponent
+{
+	virtual const EComponentType GetType() const = 0;
+};
