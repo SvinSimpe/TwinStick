0c3d90e5f2e95afe592ac590c3eab7226f883550
Simon Johanson <johanson.simon@hotmail.com>
2017-05-16 22:49:21 +0200

    Added BoxCollisionShape.

---
+++ TwinStick/BoxCollisionShape.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "CollisionShape.h"
+#include <DirectXMath.h>
+
+struct BoxCollisionShape : public CollisionShape
+{
+	float	mWidth;
+	float	mHeight;
+
+	BoxCollisionShape( DirectX::XMFLOAT2 position, float width, float height )
+	{
+		mPosition	= position;
+		mWidth		= width;
+		mHeight		= height;
+		mCenter		= DirectX::XMFLOAT2( mPosition.x + mWidth / 2.0f,
+										 mPosition.y - mHeight / 2.0f );
+	};
+
+	virtual float GetHalfExtent() const
+	{
+		return mWidth;
+	}
+
+	virtual float GetHalfExtentSq() const
+	{
+		return mWidth * mWidth;
+	}
+
+	virtual const ECollisionShape GetCollisionShape() const
+	{
+		return ECollisionShape::Box;
+
+	}
+
+};
